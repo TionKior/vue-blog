488f26176a791f7f7b140b2aef8e7bcba4469b13
1423304936@qq.com <1423304936@qq.com>
2022-01-20 11:37:49 +0800

    feat: vue-blog前台代码项目创建及路由配置

---
+++ vueblog-vue/src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import Blogs from '../views/Blogs.vue'
+import BlogEdit from '../views/BlogEdit.vue'
+import BlogDetail from '../views/BlogDetail.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Index',
+        redirect: {name: "Blogs"}
+    },
+    {
+        path: '/blogs',
+        name: 'Blogs',
+        component: Blogs
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        // 添加和编辑都是一个页面,后台判断如果没有则添加
+        // 注意放在下面有id之前,不然它可能匹配到有id的路由
+        path: '/blog/add',
+        name: 'BlogEdit',
+        component: BlogEdit
+    },
+    {
+        // 查看blog的话需要一个id,需要传入一个id
+        // 注意,查看blog放在编辑blog前面
+        path: '/blog/:blogId',
+        name: 'BlogDetail',
+        component: BlogDetail
+    },
+    {
+        // 编辑blog的话需要一个id,需要传入一个id
+        path: '/blog/:blogId/edit',
+        name: 'BlogEdit',
+        component: BlogEdit
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
---
+++ vueblog-vue/src/views/BlogEdit.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    blog-edit
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogEdit"
+}
+</script>
+
+<style scoped>
+
+</style>
